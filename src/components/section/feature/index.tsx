03c53bee182920db53992c050398fa0e2d77cd83
Daniel <danielnguyen5201@gmail.com>
2022-03-27 20:11:02 +0700

    implement function when click number of feature

--- src/components/section/feature/index.tsx
+++ src/components/section/feature/index.tsx
@@ -1,35 +1,65 @@
-import { FC } from 'react'
+import { FC, useCallback, useEffect, useState } from 'react'
 
 import AnchorTag from 'components/ui/AnchorTag'
 
-import feature01 from 'images/feature01.svg'
+import MOCK_DATA from 'data/index.json' // mock data
 import './featureSection.css'
 
+interface DataType {
+  id: string
+  nav: string
+  assests: string
+  dayChange: string
+  issued: string
+  imageUrl: string
+}
+
 const FeatureSectionComponent: FC = () => {
+  const [data, setData] = useState<DataType[]>([])
+  const [currentFeature, setCurrentFeature] = useState<DataType>({} as DataType)
+  const [currentIndex, setCurrentIndex] = useState<number>(0)
+
+  useEffect(() => {
+    setData(MOCK_DATA.data)
+    setCurrentFeature(MOCK_DATA.data[0])
+  }, [])
+
+  const changeItem = useCallback((index: number) => {
+    setCurrentFeature(data[index])
+    setCurrentIndex(index)
+  }, [data])
+
   return (
     <div className='featureSection-container'>
       <div className='featureSection-left'>
-        <h3 className='active'>01</h3>
-        <h3>02</h3>
-        <h3>03</h3>
-        <h3>04</h3>
+        {data?.map((d, i) => (
+          <>
+            <h3
+              key={d.id}
+              className={i === currentIndex ? 'active' : ''}
+              onClick={() => changeItem(i)}
+            >
+              {d.id}
+            </h3>
+          </>
+        ))}
       </div>
       <div className='featureSection-right'>
         <h1>Our Featured Funds</h1>
         <h3>Nikko AM ARM Disruptive Innovation Fund</h3>
-        <img src={feature01} alt='shape feature 01' />
+        <img src={currentFeature?.imageUrl} alt='shape feature' />
         <ul>
           <li>
-            NAV (per 100 shares) <span>¥196,977</span>
+            NAV (per 100 shares) <span>{currentFeature?.nav}</span>
           </li>
           <li>
-            Net Assests <span>7,309,193 million</span>
+            Net Assests <span>{currentFeature?.assests}</span>
           </li>
           <li>
-            Day Change <span>-1,094</span>
+            Day Change <span>{currentFeature?.dayChange}</span>
           </li>
           <li>
-            Issued <span>3,710,686,634 shares</span>
+            Issued <span>{currentFeature?.issued}</span>
           </li>
         </ul>
         <AnchorTag text='Explore more funds' color='teal' />
